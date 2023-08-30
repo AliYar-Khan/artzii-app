d308991e147a4944bcfb228fbf8dea93b20f66d2
Ali Yar Khan <aliyark145@gmail.com>
2023-08-31 00:02:11 +0500

    token expiry

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -4,10 +4,14 @@ module.exports = (req, res, next) => {
   try {
     const token = req.header("x-auth-token");
     if (!token) return res.status(403).send("Access denied.");
-
+    const currentTime = Math.floor(Date.now() / 1000);
     const decoded = jwt.verify(token, process.env.JWTPRIVATEKEY);
-    req.user = decoded;
-    next();
+    if (decoded.exp > currentTime) {
+      req.user = decoded;
+      next();
+    } else {
+      res.status(403).send({ tokenInvalid: true, message: "Token expired." });
+    }
   } catch (error) {
     res.status(400).send("Invalid token");
   }
