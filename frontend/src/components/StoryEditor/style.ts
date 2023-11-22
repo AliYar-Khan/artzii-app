adb341b50c2ddaed9fc6df14ced89f547fe3243f
Ali Yar Khan <aliyark145@gmail.com>
2023-11-23 00:47:56 +0500

    bug fix for style in story Editor

--- frontend/src/components/StoryEditor/style.ts
+++ frontend/src/components/StoryEditor/style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import Theme from '../../constants/theme'
+// import Theme from '../../constants/theme'
 
 export const Container = styled.div`
   margin-left: 0px;
