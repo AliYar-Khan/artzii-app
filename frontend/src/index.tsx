1a462304b2193a808fba02be59325a6af87eeae2
Ali Yar Khan <aliyark145@gmail.com>
2023-11-24 22:41:38 +0500

    google client id changed

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 )
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="226923677506-8kqoh0kv8nqg3opo27m11t7a19emq2gv.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="316208950929-t4tnfl1n3a4afiddtt80n9044rfdg70f.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
